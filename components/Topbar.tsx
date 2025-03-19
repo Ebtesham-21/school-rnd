1df1c330c965ac5c0c5d045d7769d18316d6f648
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-03-19 15:28:29 +0600

    topbar edit

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,12 +1,62 @@
+"use client"
+
+import { UserButton, useAuth } from '@clerk/nextjs'
+import { Search, User } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Topbar = () => {
+
+
+  const {userId} = useAuth();
+  const topRoutes = [
+    {label: "Instructor", path: "/instructor/courses"},
+    {label: "Learning", path: "/learning"}
+  ]
   return (
-    <div className='flex justify-between items-center p-4'>
-      <Link>
+    <div className='flex justify-between items-center p-12'>
+      <Link href="/"> 
+        <Image
+        src="/Asset 2.png"
+        height={200}
+        width={200}
+        alt="logo"
+      
+        />
       
       </Link>
+      <div className='max-md:hidden w-[400px] rounded-full flex'>
+         <input className='flex-grow bg-[#FFF8EB] rounded-l-full border-none outline-none text-sm pl-4 py-3' 
+         placeholder='Search for courses'
+         />
+         <button className='bg-[#FDAB04] rounded-r-full border-none outline-none cursor-pointer px-4 py-3 hover:bg-[#FDAB04]/80'>
+          <Search className='h-4 w-4'/>
+         </button>
+      </div>
+
+      <div className='flex gap-6 items-center'>
+        <div className='max-sm:hidden flex gap-6'>
+          {topRoutes.map((route) => (
+            <Link href={route.path}
+             key={route.path}
+             className='text-sm font-medium hover:text-[#FDAB04]'
+             >
+              
+              {route.label}</Link>
+          ))}
+
+
+        </div>
+        {
+          isSignedIn ? (
+            <UserButton afterSignOutUrl='sign-in'/>
+          ) : (
+
+          )
+        }
+       
+      </div>
     </div>
   )
 }
